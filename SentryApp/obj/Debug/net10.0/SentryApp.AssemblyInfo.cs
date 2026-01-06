85867b7fbf40ba0650d12a52c1c24f043fa7c7df
eric_salalima <eosalalima@gmail.com>
2026-01-06 21:39:40 +0800

    Updates

--- SentryApp/obj/Debug/net10.0/SentryApp.AssemblyInfo.cs
+++ SentryApp/obj/Debug/net10.0/SentryApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SentryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c17f433c15f3aaace1243fd3231771703093023")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2171da1bf2963d956ff6cac5a2febbcae1aeb747")]
 [assembly: System.Reflection.AssemblyProductAttribute("SentryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SentryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
